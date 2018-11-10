8bab49723d7f1d5625387b7fb6ad507f371fe41f
samjw <wagnerjsamuel@gmail.com>
2018-11-09 21:59:49 -0500

    the horror... refined js to hopefully work...

---
+++ assets/javascript/game.js
@@ -0,0 +1,58 @@
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Setting for zero
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letterUser = [];
+var eachOfLetters = null;
+
+//computer randomly selects a letter
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//number of guesses remaining, updates HTML selector
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+//letters the user has guessed so far
+function UserGuesses() {
+	document.querySelector("#letter").innerHTML = "Letters Guessed: " + letterUser.join(' ');
+}
+
+countGuessesLeft();
+
+// restart game
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+// onkeyup functions. 
+document.onkeyup = function(event) {
+	guessesLeft--;
+
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	//adds user key selection
+	letterUser.push(userGuess);
+	countGuessesLeft();
+	UserGuesses();
+
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (event.keyCode < 65 || event.keyCode > 90) {
+		alert("Invalid Entry");
+	}
+
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
+
+	
+  };
